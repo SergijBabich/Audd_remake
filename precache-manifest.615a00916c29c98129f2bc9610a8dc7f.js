994e5fbc8d8fc6fa3b3db71f9375cca1b54b86f8
SergijBabich <moran.a@list.ru>
2020-06-15 16:24:54 +0300

    Updates

---
+++ precache-manifest.615a00916c29c98129f2bc9610a8dc7f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96234fdeb23fbe942abccca25a48ef2b",
+    "url": "/Audd_remake/index.html"
+  },
+  {
+    "revision": "bbcb35feb41c7e3cfa81",
+    "url": "/Audd_remake/static/css/2.0683c493.chunk.css"
+  },
+  {
+    "revision": "b06705d359da14a90a50",
+    "url": "/Audd_remake/static/css/main.03e03461.chunk.css"
+  },
+  {
+    "revision": "bbcb35feb41c7e3cfa81",
+    "url": "/Audd_remake/static/js/2.7714d8c2.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/Audd_remake/static/js/2.7714d8c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b06705d359da14a90a50",
+    "url": "/Audd_remake/static/js/main.fb66a15c.chunk.js"
+  },
+  {
+    "revision": "f8af274c02f7cbbf693d",
+    "url": "/Audd_remake/static/js/runtime-main.ceb9b4eb.js"
+  },
+  {
+    "revision": "fb17f9762bdaa9342618cd0b4d07c763",
+    "url": "/Audd_remake/static/media/edit.fb17f976.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Audd_remake/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b8a6449fcc2675ce3db813ee5d6e1c60",
+    "url": "/Audd_remake/static/media/microphone.b8a6449f.png"
+  },
+  {
+    "revision": "67f0d277dfc27d0f4ff71cdb92dc951f",
+    "url": "/Audd_remake/static/media/rec.67f0d277.png"
+  },
+  {
+    "revision": "50735d8fb30f76cee7e1559a675d5cc4",
+    "url": "/Audd_remake/static/media/record.50735d8f.png"
+  }
+]);
